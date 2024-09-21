318708edf07ee2a35330118a3eb368a39aa1a2a8
mcrayn13 <mcrayn13@gmail.com>
2024-09-22 01:01:13 +0800

    Prep1 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // ask for name
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        String last = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
