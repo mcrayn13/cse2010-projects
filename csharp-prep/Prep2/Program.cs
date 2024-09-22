f67fdabefb1facea164b7b00fdd18cd964f54c08
mcrayn13 <mcrayn13@gmail.com>
2024-09-22 13:09:47 +0800

    Grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -9,27 +9,56 @@ class Program
         int percent = int.Parse(answer);
 
         string letter = "";
-
-        if (percent >= 90)
-        {
+        if (percent >=94)
+        {   
             letter = "A";
         }
-        else if (percent >= 80)
+
+        else if (percent >= 90)
+        {
+            letter = "A-";
+        }
+        else if (percent >= 87)
+        {
+            letter = "B+";
+        }
+        else if (percent >= 84)
         {
             letter = "B";
         }
-        else if (percent >= 70)
+         else if (percent >= 80)
+        {
+            letter = "B-";
+        }
+        else if (percent >= 77)
+        {
+            letter = "C+";
+        }
+        else if (percent >= 74)
         {
             letter = "C";
         }
-        else if (percent >= 60)
+        else if (percent >= 70)
+        {
+            letter = "C-";
+        }
+        else if (percent >= 67)
+        {
+            letter = "D+";
+        }
+        else if (percent >= 64)
         {
             letter = "D";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D-";
         }
         else
         {
             letter = "F";
         }
+        
 
         Console.WriteLine($"Your grade is: {letter}");
         
